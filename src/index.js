6574c3fbb4f2ef4898424f142e00ad9ad4284c0f
pranav prajapati <pranav0492@gmail.com>
2020-09-26 13:43:02 +0530

    port change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import renderer from "./helpers/renderer";
 
 const app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.send(renderer());
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Listening on prot 3000");
 });
