38b74b2794baf7d2ce57227e26664ea34167ffb1
pranav prajapati <pranav0492@gmail.com>
2020-09-27 14:21:23 +0530

    add material UI

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchLaunch } from "../actions";
+import { Button, Container, AppBar, Typography } from "@material-ui/core";
 
 const Home = (props) => {
   console.log("props:-------- ", props);
   return (
     <div>
+      <AppBar position="static">
+        <Typography variant="h6">SpaceX Launch Programs</Typography>
+      </AppBar>
       <div>I'm the VERY VERY BEST home component</div>
       <button onClick={() => console.log("Hi there!")}>Press me!</button>
     </div>
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -17,7 +17,10 @@ export default (req, store) => {
 
   return `
     <html>
-      <head></head>
+      <head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </head>
       <body>
         <div id="root">${content}</div>
         <script>
