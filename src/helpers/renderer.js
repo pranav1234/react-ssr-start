1669a0438a09b29e1259cb7128671c8dcf5e4493
pranav prajapati <pranav0492@gmail.com>
2020-09-30 10:56:21 +0530

    add viewport meta

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -18,6 +18,16 @@ export default (req, store) => {
   return `
     <html>
       <head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="description"
+        content="Boilerplate for React apps with routing & server side rendering"
+      />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </head>
